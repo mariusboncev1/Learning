da64beaf2d6722eaf05c1fe399c3b4135e21370a
Marius Boncev <100304041+mariusboncev1@users.noreply.github.com>
2023-04-06 12:50:29 +0300

    12.50 pm, 6/4/2023

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-prompt()
+var tweet = prompt("Compose your tweet:");
+
+var tweetCount = tweet.length;
+
+alert("You have " + tweetCount + " characters, you have " + (140 - tweetCount) + " characters remaining");
+
+var tweetSliced = tweet.slice(0,140);
+
+alert("Your final text: " + tweetSliced);
